ad41d2b4fd63a266399738b4584430eab0d61864
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-08-08 16:46:28 +0700

    Update entrypoint.sh check "$WALLET_ADDRESS"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
-/root/.nexus/bin/nexus-cli register-user --wallet-address $WALLET_ADDRESS
+
+if [ -z "$WALLET_ADDRESS" ]; then
+  echo "Error: WALLET_ADDRESS is not set."
+  exit 1
+fi
+
+/root/.nexus/bin/nexus-cli register-user --wallet-address "$WALLET_ADDRESS"
 /root/.nexus/bin/nexus-cli register-node
 /root/.nexus/bin/nexus-cli start
